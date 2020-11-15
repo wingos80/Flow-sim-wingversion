b5c7bbd349b1ed47426899d4aa7600722579c0cd
wingos80 <wingyc80@gmail.com>
2020-11-15 19:20:15 +0100

    ww

--- flow sim.py
+++ flow sim.py
@@ -12,7 +12,7 @@ pg.display.set_caption("my impeccable wave generator")
 pg.font.init()
 t = pg.time.get_ticks()*0.001
 
-# github test...
+# github test...againsss
 #################################################
 # render_mode:  0 = colour entire window with velocity gradient,
 #               1 = use particles to visualize flow on window
